e66a6e1c728e33423d0dd4c809c942b301dbc659
amandaasmac <108792107+amandaasmac@users.noreply.github.com>
2022-09-04 12:53:13 +0200

    Added github repo link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        Coded by Amanda Machado and is{" "}
+        <a
+          href="https://github.com/amandaasmac/react-weather-app"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
