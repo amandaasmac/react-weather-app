a16c794d436790bc7991149f3fbac34a2da115c1
amandaasmac <108792107+amandaasmac@users.noreply.github.com>
2022-09-04 19:51:32 +0200

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="app-container">
-        <header>
-          <button className="unit-switch">˚F</button>
-          <form className="search-form">
-            <input
-              type="search"
-              name="searchcity"
-              className="search-input"
-              placeholder="Search city..."
-            />
-            <input type="submit" value="Search" className="search-button" />
-          </form>
-          <button className="use-device-button">
-            <i className="fa-solid fa-location-dot"></i> Use device location
-          </button>
-        </header>
-        <main>
-          <div className="weather-today">
-            <h2>
-              <span>24</span>˚C
-            </h2>
-            <h1>Rome</h1>
-            <div className="today-description">Sunny</div>
-            <div className="today-min-max">
-              <span className="max-temp">30</span>˚|{" "}
-              <span className="min-temp">15</span>˚
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      maxTemp: response.data.main.temp_max,
+      minTemp: response.data.main.temp_min,
+      feelsLike: response.data.main.feels_like,
+      humidity: response.data.main.humidity,
+      windSpeed: response.data.wind.speed,
+      iconUrl: `https://aquamarine-praline-279a47.netlify.app/img/800d.png`,
+      date: `sunday 13:24`,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="app-container">
+          <header>
+            <button className="unit-switch">˚F</button>
+            <form className="search-form">
+              <input
+                type="search"
+                name="searchcity"
+                className="search-input"
+                placeholder="Search city..."
+              />
+              <input type="submit" value="Search" className="search-button" />
+            </form>
+            <button className="use-device-button">
+              <i className="fa-solid fa-location-dot"></i> Use device location
+            </button>
+          </header>
+          <main>
+            <div className="weather-today">
+              <h2>
+                <span>{Math.round(weatherData.temperature)}</span>˚C
+              </h2>
+              <h1>{weatherData.city}</h1>
+              <div className="today-description">{weatherData.description}</div>
+              <div className="today-min-max">
+                <span className="max-temp">
+                  {Math.round(weatherData.maxTemp)}
+                </span>
+                ˚|{" "}
+                <span className="min-temp">
+                  {Math.round(weatherData.minTemp)}
+                </span>
+                ˚
+              </div>
+              <div className="current-dayandtime">
+                Last updated: {weatherData.date}
+              </div>
+              <div className="today-details">
+                <div className="today-feelslike">
+                  Feels like: {Math.round(weatherData.feelsLike)}˚
+                </div>
+                <div className="today-humidity">
+                  Humidity: {weatherData.humidity}%
+                </div>
+                <div className="today-windspeed">
+                  Wind speed: {Math.round(weatherData.windSpeed)} km/h
+                </div>
+              </div>
             </div>
-            <div className="today-details">
-              <div className="today-feelslike">Feels like: 26˚</div>
-              <div className="today-humidity">Humidity: 34%</div>
-              <div className="today-windspeed">Wind speed: 12 km/h</div>
+            <div className="weather-image">
+              <img src={weatherData.iconUrl} alt={weatherData.description} />
             </div>
-          </div>
-          <div class="weather-image">
-            <img
-              src="https://aquamarine-praline-279a47.netlify.app/img/800d.png"
-              alt="sunny"
-            />
-          </div>
-        </main>
+          </main>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "f2a962d48c46d7fc23aca5910b2db6af";
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
