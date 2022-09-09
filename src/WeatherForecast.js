0ab3bc11486830c0e9404ffdcec336f9ad0f5381
amandaasmac <108792107+amandaasmac@users.noreply.github.com>
2022-09-09 16:41:47 +0200

    Api call with axios

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,6 +3,18 @@ import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast() {
+  function displayForecast(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "f2a962d48c46d7fc23aca5910b2db6af";
+  let units = "metric";
+  let longitude = 40.7;
+  let latitude = 74;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayForecast);
+
   return (
     <div className="WeatherForecast">
       <div className="daily-forecast">
